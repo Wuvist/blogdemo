d67e2211fbddfc6654f8bd75a18a16e4783ccf30
Wuvist <wuvist@gmail.com>
2020-03-17 22:38:47 +0800

    Add BlogControllerWithMockSpec

---
+++ src/test/kotlin/blogwind/com/blogapi/BlogControllerWithMockSpec.kt
@@ -0,0 +1,49 @@
+package blogwind.com.blogapi
+
+import com.blogwind.easywebmock.MockServerManager
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.test.annotation.MockBean
+import io.micronaut.test.extensions.kotlintest.MicronautKotlinTestExtension.getMock
+import io.micronaut.test.support.TestPropertyProvider
+import io.mockk.every
+import io.mockk.mockk
+import javax.inject.Inject
+
+@MicronautTest
+class BlogControllerWithMockSpec : StringSpec(), TestPropertyProvider {
+    @Inject
+    lateinit var usernameService: UsernameService
+
+    @Inject
+    @field:Client("/")
+    lateinit var client: HttpClient
+
+    init {
+        "test get blog via mock bean" {
+            MockServerManager.setOneTimeResponseJson("/blog",
+                    Blog(1, 2, "title", "bingo"))
+
+            val mock = getMock(usernameService)
+            every { mock.getUsername(any()) } returns "Jane Doe"
+
+            var rsp: String = client.toBlocking().retrieve("/blog/1")
+            rsp shouldBe "<htm><body><h1>title</h1><h2>Jane Doe</h2><p>bingo</p></body></html>"
+        }
+    }
+
+    override fun getProperties(): MutableMap<String, String> {
+        return mutableMapOf(
+                "blogapi.backend.url" to MockServerManager.getUrl()
+        )
+    }
+
+    @MockBean(UsernameService::class)
+    fun usernameService(): UsernameService {
+        return mockk()
+    }
+}
+
