ecedae4057345bee5dd884a27dc09913c2a94ad4
Wuvist <wuvist@gmail.com>
2020-06-03 22:40:28 +0800

    Handle rm error

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -8,11 +8,14 @@ GIT_HASH=`git rev-parse --short HEAD`
 ARTIFACT_VERSION=${BUILD_NUMBER}-${GIT_HASH}
 
 echo "[INFO] setting environment variable ARTIFACT VERSION: ${ARTIFACT_VERSION} for Teamcity"
+
 mkdir -p target
-rm target/*.jar
+rm -f target/*.jar
+
 mv build/libs/${APP_ID}-*-*.jar target/${APP_ID}-${ARTIFACT_VERSION}.jar
 
 echo "Generating artifact manifest for audit/verification"
+rm -f artifact_manifest.sh
 cat > artifact_manifest.sh <<EOL
 export ARTIFACT_VERSION="${ARTIFACT_VERSION}"
 EOL
