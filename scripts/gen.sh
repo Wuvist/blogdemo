14d407bb486563a50128332b66ed155f764066ae
Wuvist <wuvist@gmail.com>
2020-06-03 22:23:17 +0800

    Fix artifact_manifest.sh

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -13,7 +13,7 @@ mv build/libs/${APP_ID}-*-*.jar ${APP_ID}-${ARTIFACT_VERSION}.jar
 
 echo "Generating artifact manifest for audit/verification"
 cat > artifact_manifest.sh <<EOL
-export ARTIFACT_VERSION="{ARTIFACT_VERSION}"
+export ARTIFACT_VERSION="${ARTIFACT_VERSION}"
 EOL
 
 chmod 700 artifact_manifest.sh
